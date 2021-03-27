0593d5d28ed07b33f93fc0931183ceddaa99bfac
hadiDanial <hadidanial97@gmail.com>
2021-03-27 21:38:29 +0300

    Update Hook.cs

--- GDUJam2021/Assets/Scripts/Hook.cs
+++ GDUJam2021/Assets/Scripts/Hook.cs
@@ -6,26 +6,23 @@ using DG.Tweening;
 
 public class Hook : MonoBehaviour
 {
-    [SerializeField] private Transform holdPoint;
-    [SerializeField] private IHookable target;
+    [SerializeField] private GameObject arrow;
+    [SerializeField] private Transform holdPoint, backthrowTarget;
+    [SerializeField] private IHookable targetHookable;
     [SerializeField] private float hookDistance = 3f;
     [SerializeField] private float throwSpeed = 2.5f;
     [SerializeField] private float reelTime = 0.75f, backthowTime = 0.75f;
-    [SerializeField] private float swingRadius = 2.5f;
+    [SerializeField] private float swingRadius = 2.5f, swingSpeed = 10;
     [SerializeField] private float aimRadius = 0.2f;
     [SerializeField] private LayerMask hookLayer;
     [SerializeField] private HookState state;
 
     private Vector2 aimDirection;
-    private Transform playerTransform;
+    private Transform playerTransform, targetTransform;
     private Vector3 defaultHoldPosition;
     private Coroutine reelCoroutine;
-    bool toHold = false;
-
-    internal void SetPlayer(Transform transform)
-    {
-        playerTransform = transform;
-    }
+    bool toHold = false, forcePosition = false;
+    private float swingPercent;
 
 
     private void Start()
@@ -33,42 +30,67 @@ public class Hook : MonoBehaviour
         defaultHoldPosition = holdPoint.position;
     }
 
-    internal void SetAim(Vector2 dir)
+    internal void SetPlayer(Transform transform)
     {
-        aimDirection = dir;
+        playerTransform = transform;
+    }
+
+    internal void SetAim(Vector2 dir, bool useArrow = true)
+    {
+        aimDirection = dir.normalized;
+        if(useArrow)
+        {
+            arrow.SetActive(true);
+            arrow.transform.right = aimDirection;
+        }
+        else
+        {
+            arrow.SetActive(false);
+        }
+        backthrowTarget.localPosition = -aimDirection;
         //if (state == HookState.Held)
         //    target.SetPosition((Vector2)transform.position + aimDirection.normalized * swingRadius);
     }
 
     internal void HookObject()
     {
+        forcePosition = false;
         if (state == HookState.Empty)
         {
             toHold = true;
-            GrabObject(reelTime);
+            GrabObject(reelTime, holdPoint, false);
         }
-        else if(state == HookState.Held || state == HookState.Swinging)
-        {
-            ThrowHookedObject();
-        }
+        else 
+            ThrowHookedObject();       
     }
 
     private void ThrowHookedObject()
     {
-        target.Throw(aimDirection * throwSpeed);
-        state = HookState.Empty;
+        if (state == HookState.Held)
+        {
+            targetHookable.Throw(aimDirection * throwSpeed);
+        }
+        else if (state == HookState.Swinging)
+        {
+
+        }
+            state = HookState.Empty;
     }
 
-    private void GrabObject(float time)
+    private void GrabObject(float time, Transform targetT, bool throwObject = false)
     {
         RaycastHit2D hit = Physics2D.CircleCast(transform.position, aimRadius, aimDirection, hookDistance, hookLayer);
         if (hit)
         {
-            target = hit.collider.GetComponent<IHookable>();
-            target.Hook(transform);
+            targetHookable = hit.collider.GetComponent<IHookable>();
+            targetHookable.Hook(transform);
+            targetTransform = hit.transform;
             state = HookState.Hooked;
-            hit.transform.DOMove(holdPoint.position, time);
-            Invoke("EndGrab", time);
+            targetTransform.DOLocalMove(targetT.localPosition, time);
+            if (throwObject)
+                Invoke("Throw", time - 0.1f);
+            else
+                Invoke("EndGrab", time-0.1f);
             Debug.Log("Hit!");
         }
         else Debug.Log("No hits");
@@ -79,6 +101,7 @@ public class Hook : MonoBehaviour
         if(toHold)
         {
             toHold = false;
+            forcePosition = true;
             state = HookState.Held;
         }
 
@@ -90,16 +113,16 @@ public class Hook : MonoBehaviour
         else if (state == HookState.Swinging)
             EndSwing();
     }
+    private void StartSwing()
+    {
+        swingPercent = 0;
+    }
 
     private void EndSwing()
     {
-        throw new NotImplementedException();
+        swingPercent = 0;
     }
 
-    private void StartSwing()
-    {
-        throw new NotImplementedException();
-    }
 
     internal void Backthrow()
     {
@@ -112,11 +135,20 @@ public class Hook : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.red;
-        if(playerTransform!=null)
-        Gizmos.DrawWireSphere(playerTransform.position, hookDistance);
+        if (playerTransform != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(playerTransform.position, hookDistance);
+
+        }
+        if (backthrowTarget != null)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(backthrowTarget.position, 0.25f);
+        }
+
+        }
     }
-}
 
 public enum HookState
 {
