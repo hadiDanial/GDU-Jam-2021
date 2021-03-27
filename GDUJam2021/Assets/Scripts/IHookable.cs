74f96361e6cb188d4883e0d8c8d1059bf9c11ae0
hadiDanial <hadidanial97@gmail.com>
2021-03-27 19:21:29 +0300

    IHookable interface

---
+++ GDUJam2021/Assets/Scripts/IHookable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IHookable 
+{
+    void Hook(Transform parent);
+    void Throw(Vector2 direction);
+    void SetPosition(Vector2 position);
+    Rigidbody2D GetRB();
+    Vector2 GetPosition();
+    //void Hold(Transform holdPosition);
+
+}
