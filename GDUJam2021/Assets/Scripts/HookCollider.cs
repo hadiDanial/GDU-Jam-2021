408370813b324b94a0004a820d7813d75553b864
hadiDanial <hadidanial97@gmail.com>
2021-03-28 00:59:02 +0300

    Create HookCollider.cs
    This just lets me know when the hook head hit a target so I can reel it back in

---
+++ GDUJam2021/Assets/Scripts/HookCollider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HookCollider : MonoBehaviour
+{
+    public HookController hookController;
+    public bool canCollide = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(canCollide)
+        {
+            canCollide = false;
+            hookController.OnObjectGrabbed(collision);
+        }
+    }
+}
