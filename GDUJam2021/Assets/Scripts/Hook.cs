4cb032790f2bc71e46fde86a5dfbb2c7f625d466
hadiDanial <hadidanial97@gmail.com>
2021-03-28 00:58:37 +0300

    Update Hook.cs

--- GDUJam2021/Assets/Scripts/Hook.cs
+++ GDUJam2021/Assets/Scripts/Hook.cs
@@ -26,29 +26,22 @@ public class Hook : MonoBehaviour
     {
         if(controller == null) controller = GetComponent<SpriteShapeController>();
 
-        //controller.spline.Clear();
-
         controller.spline.SetPosition(0, splinePositions[0]);
         controller.spline.SetPosition(1, splinePositions[1]);
-        //controller.spline.SetTangentMode(0, ShapeTangentMode.Linear);
-        //controller.spline.SetTangentMode(1, ShapeTangentMode.Broken);
+
         controller.spline.SetSpriteIndex(0, 1);
         controller.spline.SetSpriteIndex(1, 0);
         for (int i = 2; i < splinePositions.Length - 2; i++)
         {
-            //controller.spline.SetRightTangent(i - 1, (splinePositions[i] - splinePositions[i - 1])/2f);
             controller.spline.SetPosition(i, splinePositions[i]);
-            //controller.spline.SetTangentMode(i, ShapeTangentMode.Continuous);
-            //controller.spline.SetLeftTangent(i, splinePositions[i + 1] - splinePositions[i]);
+
         }
 
-        int lastTwo = splinePositions.Length - 2;
-        controller.spline.SetPosition(lastTwo, splinePositions[lastTwo]);
-        //controller.spline.SetTangentMode(lastTwo, ShapeTangentMode.Broken);
-        controller.spline.SetPosition(lastTwo, splinePositions[lastTwo] - (splinePositions[lastTwo-1] - splinePositions[lastTwo]).normalized*0.5f);
-        controller.spline.SetSpriteIndex(lastTwo, 2);
-        lastTwo++;
-        //controller.spline.SetTangentMode(lastTwo, ShapeTangentMode.Linear);
+        int index = splinePositions.Length - 2;
+        controller.spline.SetPosition(index, splinePositions[index]);
+
+        controller.spline.SetPosition(index, splinePositions[index] - (splinePositions[index-1] - splinePositions[index]).normalized*0.5f);
+        controller.spline.SetSpriteIndex(index, 2);
 
         controller.RefreshSpriteShape();
     }
