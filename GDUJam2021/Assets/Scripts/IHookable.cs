c99ac441eb870e9739f0a3289f273351c7bc0855
hadiDanial <hadidanial97@gmail.com>
2021-03-29 00:04:09 +0300

    Update IHookable.cs
    Added Hold function

--- GDUJam2021/Assets/Scripts/IHookable.cs
+++ GDUJam2021/Assets/Scripts/IHookable.cs
@@ -7,6 +7,7 @@ public interface IHookable
     void Hook(Transform parent);
     void Throw(Vector2 direction);
     void SetPosition(Vector2 position);
+    void Hold();
     Rigidbody2D GetRB();
     Vector2 GetPosition();
     //void Hold(Transform holdPosition);
