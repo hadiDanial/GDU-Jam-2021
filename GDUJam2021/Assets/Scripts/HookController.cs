779d0ab478e78e98099c6cedc31f20fbe061294d
hadiDanial <hadidanial97@gmail.com>
2021-03-29 00:03:16 +0300

    Update HookController.cs

--- GDUJam2021/Assets/Scripts/HookController.cs
+++ GDUJam2021/Assets/Scripts/HookController.cs
@@ -8,36 +8,41 @@ using DG.Tweening;
 public class HookController : MonoBehaviour
 {
     [SerializeField] private Hook hook;
-    [SerializeField] private GameObject hookCollider;
-    [SerializeField] private SpriteRenderer hookHeadRenderer;
-    [SerializeField] private GameObject arrow;
-    [SerializeField] private Transform holdPoint, backthrowTarget;
+    [SerializeField] private SpriteRenderer hookHeadRenderer = null;
+    [SerializeField] private GameObject hookGameObject = null, hookPos = null;
+    [SerializeField] protected Transform holdPoint, backthrowTarget;
     [SerializeField] private IHookable targetHookable;
     [SerializeField] private float hookDistance = 3f;
     [SerializeField] private float throwSpeed = 2.5f;
     [SerializeField] private float reelTime = 0.75f, backthowTime = 0.75f;
     [SerializeField] private float swingRadius = 2.5f, swingSpeed = 10;
-    [SerializeField] private float aimRadius = 0.2f;
+    [SerializeField] private float aimRadius = 0.3f;
     [SerializeField] private LayerMask hookLayer;
     [SerializeField] private HookState state;
 
+    internal PlayerController playerController;
     private Vector2 aimDirection;
     private Transform playerTransform, targetTransform;
     private Vector3 defaultHoldPosition;
-    bool toHold = false, forcePosition = false;
-    private float swingPercent;
-
-
+    private float swingPercent, currentTime;
+    private RaycastHit2D hit;
+    private Transform targetT;
+    internal bool throwObject;
+    private bool lockBackthrowTarget;
+    float hitPosX, hitPosY;
     private void Start()
     {
+        //hook.canCollide = false;
+        //hook.hookController = this;
+        hook.Setup(hookLayer, aimRadius, this);
+        hitPosX = hookPos.transform.localPosition.x;
+        hitPosY = hookPos.transform.localPosition.y;
         defaultHoldPosition = holdPoint.position;
+        playerController = GetComponent<PlayerController>();
     }
     private void Update()
     {
-        if(forcePosition && state == HookState.Held)
-        {
-            targetTransform.position = holdPoint.position;
-        }
+
     }
     internal void SetPlayer(Transform transform)
     {
@@ -49,80 +54,136 @@ public class HookController : MonoBehaviour
         aimDirection = dir.normalized;
         if(useArrow)
         {
-            arrow.SetActive(true);
+            hookGameObject.SetActive(true);
             //arrow.transform.right = aimDirection;
-            arrow.transform.rotation = MathP.RotationFromDirection(-aimDirection);
-            if (aimDirection.x < 0) hookHeadRenderer.flipY = true;
-            else if (aimDirection.x > 0) hookHeadRenderer.flipY = false;
+            hookGameObject.transform.rotation = MathP.RotationFromDirection(-aimDirection);
+            if (aimDirection.x < 0)
+            {
+                hookHeadRenderer.flipY = true;
+                hookPos.transform.localPosition = new Vector2(hitPosX, -hitPosY);
+            }
+            else if (aimDirection.x > 0)
+            {
+                hookHeadRenderer.flipY = false;
+                hookPos.transform.localPosition = new Vector2(hitPosX, hitPosY);
+            }
         }
         else
         {
-            arrow.SetActive(false);
+            hookGameObject.SetActive(false);
         }
-        backthrowTarget.localPosition = -aimDirection;
+        if (playerController._isGrounded)
+            aimDirection = new Vector2(aimDirection.x, Mathf.Clamp(aimDirection.y, 0, 2));
+        if (!lockBackthrowTarget)
+            backthrowTarget.localPosition = -aimDirection * hookDistance;
         //if (state == HookState.Held)
         //    target.SetPosition((Vector2)transform.position + aimDirection.normalized * swingRadius);
     }
 
+    /// <summary>
+    /// Hook an object or throw it
+    /// </summary>
     internal void HookObject()
     {
-        forcePosition = false;
         if (state == HookState.Empty)
         {
-            toHold = true;
-            GrabObject(reelTime, holdPoint, false);
+            targetT = holdPoint;
+            currentTime = reelTime;
+            throwObject = false;
+            GrabObject();
         }
         else 
-            ThrowHookedObject();       
+            ThrowHookedObject(aimDirection);       
     }
 
-    private void ThrowHookedObject()
+    /// <summary>
+    /// Try to grab object
+    /// </summary>
+    private void GrabObject()
     {
-        if (state == HookState.Held)
+        hook.ThrowHead(aimDirection, hookDistance, hook, currentTime, targetT);
+    }
+
+    /// <summary>
+    /// What to do when the object is grabbed
+    /// </summary>
+    /// <param name="collision"></param>
+    internal void OnObjectGrabbed(Collider2D hookable)
+    {
+        targetHookable = hookable.GetComponent<IHookable>();
+        targetHookable.Hook(hookPos.transform);
+        targetTransform = hookable.transform;
+        state = HookState.Hooked;
+    }
+    
+    /// <summary>
+    /// Throw object
+    /// </summary>
+    private void ThrowHookedObject(Vector2 direction)
+    {
+        if (targetHookable == null)
         {
-            targetHookable.Throw(aimDirection * throwSpeed);
+            Clear();
+            return;
+        }
+        else if (state == HookState.Held || throwObject)
+        {
+            if (targetHookable != null)
+            {
+                targetHookable.Throw(direction * throwSpeed);
+                playerController.audioSource.PlayOneShot(playerController.throwSound);
+            }
+            Clear();
+            if (throwObject) throwObject = false;
+            lockBackthrowTarget = false;
         }
         else if (state == HookState.Swinging)
         {
 
         }
-            state = HookState.Empty;
+        state = HookState.Empty;
     }
 
-    private void GrabObject(float time, Transform targetT, bool throwObject = false)
+    internal void Clear()
     {
-        RaycastHit2D hit = Physics2D.CircleCast(transform.position, aimRadius, aimDirection, hookDistance, hookLayer);
-        if (hit)
-        {
-            ThrowHook(time, targetT, throwObject, hit);
-        }
-        else Debug.Log("No hits");
+        targetHookable = null;
+        targetTransform = null;
+        state = HookState.Empty;
     }
 
-    private void ThrowHook(float time, Transform targetT, bool throwObject, RaycastHit2D hit)
+    internal void OnReelFinished()
     {
-        targetHookable = hit.collider.GetComponent<IHookable>();
-        targetHookable.Hook(transform);
-        targetTransform = hit.transform;
-        state = HookState.Hooked;
-        targetTransform.DOLocalMove(targetT.localPosition, time);
-        if (throwObject)
-            Invoke("Throw", time - 0.1f);
-        else
-            Invoke("EndGrab", time - 0.1f);
-        Debug.Log("Hit!");
+        if (targetHookable != null)
+            targetHookable.Hold();
+        EndGrab();
     }
 
     private void EndGrab()
     {
-        if(toHold)
+        if(targetHookable != null)
         {
-            toHold = false;
-            forcePosition = true;
             state = HookState.Held;
+        }
+        else
+        {
+            throwObject = false;
+            lockBackthrowTarget = false;
         }
 
     }
+
+    internal void OnBackreelFinished()
+    {
+        if (targetHookable != null)
+        {
+            targetHookable.Hold();
+            if (throwObject)
+            {
+                ThrowHookedObject(-aimDirection);
+            }
+        }
+    }
+
     internal void Swing()
     {
         if (state == HookState.Held)
@@ -145,7 +206,12 @@ public class HookController : MonoBehaviour
     {
         if(state == HookState.Empty)
         {
-            GrabObject(backthowTime, backthrowTarget, true);
+            lockBackthrowTarget = true;
+            targetT = backthrowTarget;
+            currentTime = backthowTime;
+            throwObject = true;
+            
+            GrabObject();
         }
     }
 
